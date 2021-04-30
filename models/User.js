0e7ecbeb7e7215f81516b76133404ba4fa48e969
Callie Nipper <callie.nipper@gmail.com>
2021-04-29 19:55:00 -0700

    Created User model

---
+++ models/User.js
@@ -0,0 +1,67 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        //define Username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+
+        //define password
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // password must be at least five characters long
+            validate: {
+                len: [5],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        //imported sequelize connection (the direct connection to database)
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+model.exports = User;
