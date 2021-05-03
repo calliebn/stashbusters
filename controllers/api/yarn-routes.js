afe55ee89c32520654d8ffca61a43b3251333905
Callie Nipper <callie.nipper@gmail.com>
2021-05-03 16:02:18 -0700

    Created both of the GET routes for the yarn-routes.js file in the api folder

--- controllers/api/yarn-routes.js
+++ controllers/api/yarn-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { Yarn } = require('../../models');
+const { Yarn, User } = require('../../models');
 
 const withAuth = require('../../utils/auth');
 
+// Creates the yarn cards
 router.post('/', withAuth, async (req, res) => {
   try {
     const newYarn = await Yarn.create({
@@ -16,6 +17,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// deletes the yarn cards
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const newYarn = await Yarn.destroy({
@@ -26,7 +28,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!newYarn) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No yarn found with this id!' });
       return;
     }
 
@@ -35,5 +37,69 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Gets the yarn cards
+router.get('/', (req, res) => {
+  Yarn.findAll({
+    attributes: [
+      'company',
+      'brand',
+      'colorway',
+      'yardage',
+      'grams',
+      'weight',
+      'skeins',
+      'dye_lot'
+    ]
+
+  })
+    .then(dbYarnData => res.json(dbYarnData.reverse()))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
+});
+
+// Gets one yarn
+router.get('/:id', (req, res) => {
+  Yarn.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['company', 'brand', 'colorway', 'yardage', 'grams', 'weight', 'skeins', 'dye_lot']
+  })
+})
+
+  .then(dbYarnData => {
+    if (!dbYarnData) {
+      res.status(404).json({ message: 'This yarn cannot be found' });
+      return;
+    }
+    res.json(dbYarnData);
+  })
+
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
+// Updates an existing yarn card
+router.put('/:id', withAuth, (req, res) => {
+  Yarn.update({
+    comapny: req.params.company,
+    brand: req.params.brand,
+    colorway: req.params.colorway,
+    yardage: req.params.yardage,
+    grams: req.params.grams,
+    weight: req.params.weight,
+    skeins: req.params.skeins,
+    dye_lot: req.params.dye_lot
+  },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+})
 
 module.exports = router;
