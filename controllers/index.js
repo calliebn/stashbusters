c9bea5461f720d5d787da33e59ef42d1338e424f
Callie Nipper <callie.nipper@gmail.com>
2021-05-04 19:03:53 -0700

    Tweaked home-routes and index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 
+const { endsWith } = require('sequelize/types/lib/operators');
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes.js');
+const yarnRoutes = require('./yarn-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/yarn', yarnRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+})
+
 
 module.exports = router;
