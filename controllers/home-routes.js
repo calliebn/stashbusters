ca097ecca95909b7e5e79e35fc233f55446a1284
Goldiluck Rebollido <gorebollido@gmail.com>
2021-05-04 20:29:24 -0700

    ignore this files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,42 +1,43 @@
-const router = require('express').Router();
-const { User, Yarn } = require('../models');
+const router = require("express").Router();
+const { User, Yarn } = require("../models");
 const sequelize = require('../config/connection');
 
 // Homepage
-router.get('/', async (req, res) => {
-  try {
-    const yarnData = await Post.findAll({
-      attributes: ['id'],
-      include: [
-        {
-          model: Yarn,
-          attributes: [
-            'company',
-            'brand',
-            'colorway',
-            'yardage',
-            'grams',
-            'weight',
-            'skeins',
-            'dye_lot',
-            'user_id',
-          ],
-          include: { model: User, attributes: ['id'] },
-        },
-      ],
-    });
+router.get("/", async (req, res) => {
+  // try {
+  //   const yarnData = await Yarn.findAll({
+  //     attributes: ["id"],
+  //     include: [
+  //       {
+  //         model: Yarn,
+  //         attributes: [
+  //           "company",
+  //           "brand",
+  //           "colorway",
+  //           "yardage",
+  //           "grams",
+  //           "weight",
+  //           "skeins",
+  //           "dye_lot",
+  //           "user_id",
+  //         ],
+  //         include: { model: User, attributes: ["id"] },
+  //       },
+  //     ],
+  //   });
 
     // Serialize data so the template can read it
     const yarns = yarnData.map((yarn) => yarn.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', {
-      yarns,
-      logged_in: req.session.logged_in,
+    res.render("homepage", {
+      // yarns,
+      // logged_in: req.session.logged_in,
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    console.log("error in codes", err);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 module.exports = router;
