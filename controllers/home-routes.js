bdf17bd749e577158f9b10c86e21c5e0c4501d69
Goldiluck Rebollido <gorebollido@gmail.com>
2021-05-04 21:01:01 -0700

    accepted changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,41 +3,9 @@ const { User, Yarn } = require("../models");
 const sequelize = require('../config/connection');
 
 // Homepage
-router.get("/", async (req, res) => {
-  // try {
-  //   const yarnData = await Yarn.findAll({
-  //     attributes: ["id"],
-  //     include: [
-  //       {
-  //         model: Yarn,
-  //         attributes: [
-  //           "company",
-  //           "brand",
-  //           "colorway",
-  //           "yardage",
-  //           "grams",
-  //           "weight",
-  //           "skeins",
-  //           "dye_lot",
-  //           "user_id",
-  //         ],
-  //         include: { model: User, attributes: ["id"] },
-  //       },
-  //     ],
-  //   });
+router.get('/', async (req, res) => {
 
-    // Serialize data so the template can read it
-    const yarns = yarnData.map((yarn) => yarn.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render("homepage", {
-      // yarns,
-      // logged_in: req.session.logged_in,
-    });
-    console.log("error in codes", err);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+  res.render('homepage');
 });
 
 module.exports = router;
