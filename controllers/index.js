76d5c2ca0d8e60444b45fee72162e1ee98842d31
Kp <kp_1@outlook.com>
2021-05-05 06:05:34 +0400

    Updates

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const yarnRoutes = require('./yarn-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/yarn', yarnRoutes);
 
 module.exports = router;
