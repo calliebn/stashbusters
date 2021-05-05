04e2bf2f9b1b9f629d4397e14566051f56f43af3
Callie Nipper <callie.nipper@gmail.com>
2021-05-04 20:13:26 -0700

    route fixes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,39 +4,8 @@ const sequelize = require('../config/connection');
 
 // Homepage
 router.get('/', async (req, res) => {
-  try {
-    const yarnData = await Post.findAll({
-      attributes: ['id'],
-      include: [
-        {
-          model: Yarn,
-          attributes: [
-            'company',
-            'brand',
-            'colorway',
-            'yardage',
-            'grams',
-            'weight',
-            'skeins',
-            'dye_lot',
-            'user_id',
-          ],
-          include: { model: User, attributes: ['id'] },
-        },
-      ],
-    });
 
-    // Serialize data so the template can read it
-    const yarns = yarnData.map((yarn) => yarn.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      yarns,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render('homepage');
 });
 
 module.exports = router;
