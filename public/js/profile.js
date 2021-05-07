f33da5067611331353af753195ac41a8e1868f55
Kp <kp_1@outlook.com>
2021-05-07 19:31:32 +0400

    handlebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,7 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
+  <h1>Add some some to yarn sure the link works</h1>;
+
   const yarn = document.querySelector('#yarn-name').value.trim();
 };
