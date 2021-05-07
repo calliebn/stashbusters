0b3ef7af6a738e4265d3c1c728e2b7c74d0f28b6
Callie Nipper <callie.nipper@gmail.com>
2021-05-07 07:28:57 -0700

    Fixed signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,18 @@
 async function signupFormHandler(event) {
   event.preventDefault();
-
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
 
   if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         username,
         password,
+        email
       }),
-      headers: { 'Content-Type': 'application/json' },
     });
     if (response.ok) {
       console.log('success');
