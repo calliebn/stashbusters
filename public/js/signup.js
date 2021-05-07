8effd57b4cef0d233ad1f275dd4b8b7d3542c74c
Kp <kp_1@outlook.com>
2021-05-07 20:58:57 +0400

    signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,14 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && password) {
-    const response = await fetch('/api/users', {
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({
-        username,
+        email,
         password,
       }),
       headers: { 'Content-Type': 'application/json' },
@@ -24,5 +24,5 @@ async function signupFormHandler(event) {
 }
 
 document
-  .querySelector('#signup-form')
+  .querySelector('#signup-button')
   .addEventListener('submit', signupFormHandler);
